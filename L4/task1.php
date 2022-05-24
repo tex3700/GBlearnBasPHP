314c1e60686da846481ff31f9a520b0ce821a7de
tex3700 <tex3700@gmail.com>
2022-05-24 12:34:35 +0300

    some change from hw show

--- L4/task1.php
+++ L4/task1.php
@@ -4,13 +4,7 @@ $arr = [4, 5, 1, 4, 7, 8, 15, 6, 71, 45, 2, 0];
 
 $evenOdd = function (int $number) : string {
 
-    if ( $number == 0 ) {
-        return "{$number} - Ноль";
-    } elseif ( $number & 1 ) {
-        return "{$number} - Нечетное";
-    } else {
-        return "{$number} - Четное";
-    }
+    return $number & 1 ? 'Нечетное': 'Четное';
 };
 
 $result = array_map( $evenOdd, $arr );
--- L4/task2.php
+++ L4/task2.php
@@ -4,13 +4,11 @@ $numArr = [4, 5, 1, 4, 7, 8, 15, 6, 71, 45, 2, 0];
 
 function minMaxAverage (array $arr) :array
 {
-    asort($arr);
-    $avg = array_sum($arr) / count($arr);
 
     $resultArray = [
-        'min' => current($arr),
-        'max' => end($arr),
-        'avg' => $avg
+        'min' => min($arr),
+        'max' => max($arr),
+        'avg' => array_sum($arr) / count($arr)
     ];
 
     return $resultArray;
